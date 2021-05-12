229df6b37dc52f1a358b2488b24cee1c8eb5a212
yihu wang <wangyihu2009@msn.com>
2021-05-12 17:36:25 +0800

    修改

---
+++ ClientSample/ProducerWorker.cs
@@ -0,0 +1,144 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Subscribing;
+using MQTTnet.Client.Unsubscribing;
+using MQTTnet.Packets;
+
+namespace ClientSample
+{
+    public class ProducerWorker : BackgroundService
+    {
+        private readonly ILogger<ProducerWorker> _logger;
+        private readonly MqttTopicFilter _mqttTopicFilter;
+        private readonly IMqttClientOptions _options;
+        private readonly IMqttFactory _mqttFactory;
+        private IMqttClient _mqttClient;
+        private readonly MqttUserProperty _mqttUserProperty;
+
+        const string producerTopic = "jieke/wang/producer";
+        const string consumerTopic = "jieke/wang/consumer";
+        const string username = "jieke";
+        const string password = "wang";
+
+        public ProducerWorker(ILogger<ProducerWorker> logger)
+        {
+            _logger = logger;
+            _options = new MqttClientOptionsBuilder()
+                .WithClientId(Guid.NewGuid().ToString())
+                .WithCredentials(username, password)
+                .WithCleanSession()
+                .WithTcpServer(options =>
+                {
+                    options.Server = "127.0.0.1";
+                    options.Port = 6666;
+                })
+                .Build();
+            _mqttFactory = new MqttFactory();
+
+            _mqttTopicFilter = new MqttTopicFilter
+            {
+                Topic = producerTopic,
+                QualityOfServiceLevel = MQTTnet.Protocol.MqttQualityOfServiceLevel.AtMostOnce
+            };
+
+            _mqttUserProperty = new MqttUserProperty(username, password);
+        }
+
+        public override async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _mqttClient = _mqttFactory.CreateMqttClient();
+            _mqttClient.UseDisconnectedHandler(async arg =>
+            {
+                if (cancellationToken.IsCancellationRequested || _mqttClient.IsConnected) return;
+                await _mqttClient.ReconnectAsync();
+            });
+            _mqttClient.UseConnectedHandler(arg => 
+            {
+                _logger.LogInformation($"连接状态: {arg.AuthenticateResult.ResultCode}");
+            });
+
+            _mqttClient.UseApplicationMessageReceivedHandler(arg =>
+            {
+                //Console.WriteLine("\n\n");
+                //_logger.LogInformation("===Producer===");
+                //_logger.LogInformation($"接收来自 [{arg.ClientId}] 消息");
+                //_logger.LogInformation($"主题 [{arg.ApplicationMessage.Topic}]");
+                //_logger.LogInformation($"回复主题 [{arg.ApplicationMessage.ResponseTopic}]");
+                //_logger.LogInformation($"消息: {Encoding.UTF8.GetString(arg.ApplicationMessage.Payload)}");
+                //Console.WriteLine("\n\n");
+
+                StringBuilder message = new StringBuilder();
+                message.AppendLine($"\n\n===Producer===");
+                message.AppendLine($"接收来自 [{arg.ClientId}] 消息");
+                message.AppendLine($"主题 [{arg.ApplicationMessage.Topic}]");
+                message.AppendLine($"消息: {Encoding.UTF8.GetString(arg.ApplicationMessage.Payload)}");
+                message.Append($"\n\n");
+                _logger.LogInformation(message.ToString());
+
+                arg.ProcessingFailed = false;
+                arg.ReasonCode = MqttApplicationMessageReceivedReasonCode.Success;
+                arg.IsHandled = true;
+            });
+
+            await _mqttClient.ConnectAsync(_options, cancellationToken);
+            await _mqttClient.SubscribeAsync(new MqttClientSubscribeOptions
+            {
+                TopicFilters = new List<MqttTopicFilter> { _mqttTopicFilter },
+                UserProperties = new List<MqttUserProperty> { _mqttUserProperty }
+            }, cancellationToken);
+
+            await base.StartAsync(cancellationToken);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    //await _mqttClient.PingAsync(stoppingToken);
+                    string msg = $"{Environment.ProcessId} {Guid.NewGuid()} {DateTime.Now}";
+                    Console.WriteLine($"\n\n发送消息: {msg}\n\n");
+                    await _mqttClient.PublishAsync(new MqttApplicationMessage
+                    {
+                        Topic = consumerTopic,
+                        Payload = Encoding.UTF8.GetBytes(msg),
+                        QualityOfServiceLevel = MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce,
+                        ResponseTopic = producerTopic,
+                        UserProperties = new List<MqttUserProperty> { _mqttUserProperty }
+                    }, stoppingToken);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, ex.Message);
+                }
+
+                //await Task.Delay(5000, stoppingToken);
+                //await Task.Delay(999999, stoppingToken);
+            }
+        }
+
+        public override async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await _mqttClient?.UnsubscribeAsync(new MqttClientUnsubscribeOptions
+            {
+                TopicFilters = new List<string> { _mqttTopicFilter.Topic }
+            }, cancellationToken);
+            await _mqttClient?.DisconnectAsync(cancellationToken);
+            _mqttClient?.Dispose();
+
+            await base.StopAsync(cancellationToken);
+        }
+    }
+}
---
+++ ServerSample/Worker.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using MQTTnet;
+using MQTTnet.Protocol;
+using MQTTnet.Server;
+
+namespace ServerSample
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly IMqttServerOptions _mqttServerOptions;
+        private readonly IMqttFactory _mqttFactory;
+        private IMqttServer _mqttServer;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+
+            _mqttServerOptions = new MqttServerOptionsBuilder()
+                .WithDefaultEndpointPort(6666)
+                .WithConnectionValidator(context => 
+                {
+                    if(context.ClientId.Length < 10)
+                    {
+                        context.ReasonCode = MqttConnectReasonCode.ClientIdentifierNotValid;
+                        return;
+                    }
+
+                    if(context.Username != "jieke" || context.Password != "wang")
+                    {
+                        context.ReasonCode = MqttConnectReasonCode.NotAuthorized;
+                        return;
+                    }
+
+                    context.ReasonCode = MqttConnectReasonCode.Success;
+                })
+                .Build();
+            _mqttFactory = new MqttFactory();
+        }
+
+        public override async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _mqttServer = _mqttFactory.CreateMqttServer();
+
+            // 客户端连接
+            _mqttServer.UseClientConnectedHandler(arg =>
+            {
+                _logger.LogInformation($"\n\n{arg.ClientId} 已连接\n\n");
+            });
+            // 客户端断开
+            _mqttServer.UseClientDisconnectedHandler(arg =>
+            {
+                _logger.LogInformation($"\n\n{arg.ClientId} 已断开, 类型为: {arg.DisconnectType}\n\n");
+            });
+            // 接收客户端发来的消息
+            _mqttServer.UseApplicationMessageReceivedHandler(arg =>
+            {
+                //Console.WriteLine("\n\n");
+                //_logger.LogInformation("===Server===");
+                //_logger.LogInformation($"接收来自 [{arg.ClientId}] 消息");
+                //_logger.LogInformation($"主题 [{arg.ApplicationMessage.Topic}]");
+                //_logger.LogInformation($"回复主题 [{arg.ApplicationMessage.ResponseTopic}]");
+                //_logger.LogInformation($"消息: {Encoding.UTF8.GetString(arg.ApplicationMessage.Payload)}");
+                //Console.WriteLine("\n\n");
+
+                StringBuilder message = new StringBuilder();
+                message.AppendLine($"\n\n===Server===");
+                message.AppendLine($"接收来自 [{arg.ClientId}] 消息");
+                message.AppendLine($"主题 [{arg.ApplicationMessage.Topic}]");
+                message.AppendLine($"消息: {Encoding.UTF8.GetString(arg.ApplicationMessage.Payload)}");
+                message.Append($"\n\n");
+                _logger.LogInformation(message.ToString());
+
+                //if (string.IsNullOrWhiteSpace(arg.ApplicationMessage.ResponseTopic) == false)
+                //{
+                //    _mqttServer.PublishAsync(new MqttApplicationMessage 
+                //    {
+                //        Topic = arg.ApplicationMessage.ResponseTopic,
+                //        Payload = arg.ApplicationMessage.Payload,
+                //        QualityOfServiceLevel = arg.ApplicationMessage.QualityOfServiceLevel,
+                //        ResponseTopic = arg.ApplicationMessage.Topic,
+                //        UserProperties = arg.ApplicationMessage.UserProperties
+                //    }, cancellationToken);
+                //}
+
+                arg.ProcessingFailed = false;
+                arg.ReasonCode = MqttApplicationMessageReceivedReasonCode.Success;
+                arg.IsHandled = true;
+            });
+            await base.StartAsync(cancellationToken);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await _mqttServer.StartAsync(_mqttServerOptions);
+        }
+
+        public override async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await _mqttServer?.ClearRetainedApplicationMessagesAsync();
+            await _mqttServer?.StopAsync();
+            _mqttServer?.Dispose();
+            await base.StopAsync(cancellationToken);
+        }
+    }
+}
